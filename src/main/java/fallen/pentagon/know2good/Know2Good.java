6b3e8e342f510d05b870f56e70f86a518466050f
joshua <joshua.jacobowitz@yahoo.de>
2020-03-15 20:53:21 +0100

    Add reload command

--- src/main/java/fallen/pentagon/know2good/Know2Good.java
+++ src/main/java/fallen/pentagon/know2good/Know2Good.java
@@ -1,5 +1,7 @@
 package fallen.pentagon.know2good;
 
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class Know2Good extends JavaPlugin {
@@ -8,5 +10,16 @@ public class Know2Good extends JavaPlugin {
     getServer().getPluginManager().registerEvents(new PlayerInteractListener(this), this);
     getConfig().options().copyDefaults(true);
     saveConfig();
+  }
+
+  @Override
+  public boolean onCommand(
+    CommandSender _sender,
+    Command _command,
+    String _label,
+    String[] _args
+  ) {
+    reloadConfig();
+    return true;
   }
 }
