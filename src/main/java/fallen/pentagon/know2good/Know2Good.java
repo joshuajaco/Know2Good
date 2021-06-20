776b743517547ec7da89f47357bdd46635cced38
Joshua <joshua.jacobowitz@yahoo.de>
2021-06-20 16:45:51 +0200

    Add teleport configuration

--- src/main/java/fallen/pentagon/know2good/Know2Good.java
+++ src/main/java/fallen/pentagon/know2good/Know2Good.java
@@ -12,19 +12,39 @@ public class Know2Good extends JavaPlugin {
     getServer().getPluginManager().registerEvents(new PlayerItemConsumeListener(this), this);
     getConfig().options().copyDefaults(true);
     saveConfig();
+
+    var teleportConfig = getConfig().getConfigurationSection("teleport");
+
+    if (teleportConfig == null) return;
+
+    var checkEvery = teleportConfig.getLong("check_every_n_ticks");
+
+    var overWorldConfig = teleportConfig.getConfigurationSection("overworld_to_end");
+
+    if (overWorldConfig == null) return;
+
+    var overWorldFrom = overWorldConfig.getInt("from");
+    var overWorldTo = overWorldConfig.getInt("to");
+
+    var endConfig = teleportConfig.getConfigurationSection("end_to_overworld");
+
+    if (endConfig == null) return;
+
+    var endFrom = endConfig.getInt("from");
+    var endTo = endConfig.getInt("to");
+
+    var overWorld = getServer().getWorlds().get(0);
+    var end = getServer().getWorlds().get(2);
     getServer().getScheduler().scheduleSyncRepeatingTask(this, new Runnable() {
       @Override
       public void run() {
-        var overWorld = getServer().getWorlds().get(0);
-        var end = getServer().getWorlds().get(2);
-
         overWorld.getPlayers().forEach(player -> {
           var location = player.getLocation();
-          if (location.getBlockY() > 320) {
+          if (location.getBlockY() > overWorldFrom) {
             var velocity = player.getVelocity().clone();
             var newLocation = location.clone();
             newLocation.setWorld(end);
-            newLocation.setY(10);
+            newLocation.setY(overWorldTo);
             player.teleport(newLocation);
             player.setVelocity(velocity);
             player.sendMessage("Teleported to end");
@@ -33,18 +53,18 @@ public class Know2Good extends JavaPlugin {
 
         end.getPlayers().forEach(player -> {
           var location = player.getLocation();
-          if (location.getBlockY() < -5) {
+          if (location.getBlockY() < endFrom) {
             var velocity = player.getVelocity().clone();
             var newLocation = location.clone();
             newLocation.setWorld(overWorld);
-            newLocation.setY(320);
+            newLocation.setY(endTo);
             player.teleport(newLocation);
             player.setVelocity(velocity);
             player.sendMessage("Teleported to overworld");
           }
         });
       }
-    }, 0L, 20L);
+    }, 0L, checkEvery);
 
   }
 
